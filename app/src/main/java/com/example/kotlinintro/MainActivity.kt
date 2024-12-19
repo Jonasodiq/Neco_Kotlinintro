de7bd314fd1dc12405cf1eb75c2e0281649f1864
Jonas N <jonasniyazson@hotmail.com>
2024-12-19 18:54:51 +0100

    Bismillah lifecycle

--- app/src/main/java/com/example/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/kotlinintro/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.kotlinintro
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,23 +19,39 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Log.d("MyLogMAct", "onCreate")
     }
 
 
     override fun onStart() {
         super.onStart()
+        Log.d("MyLogMAct", "onStart")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d("MyLogMAct", "onResume")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d("MyLogMAct", "onPause")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d("MyLogMAct", "onDestroy")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MyLogMAct", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MyLogMAct", "onRestart")
     }
 }
 
