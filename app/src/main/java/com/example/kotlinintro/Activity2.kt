943d14fedc5a37f055909ea3f398dfd6d3ddf77a
Jonas N <jonasniyazson@hotmail.com>
2024-12-19 18:24:08 +0100

    Bismillah

--- app/src/main/java/com/example/kotlinintro/Activity2.kt
+++ app/src/main/java/com/example/kotlinintro/Activity2.kt
@@ -6,12 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
+class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -19,27 +18,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
 }
 
-/*
-* Linl https://www.youtube.com/watch?v=foTZ4rzCFKc
-* The activity lifecycle -> https://developer.android.com/guide/components/activities/activity-lifecycle?hl=ru
-*
-*
-*  */
