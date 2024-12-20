0f070e342f0c2a7cd77e0caec9269ab8c21bb1d6
Jonas N <jonasniyazson@hotmail.com>
2024-12-20 11:14:35 +0100

    Bismillah -> Alhamdu lillah -> Added Temp

--- app/src/main/java/com/example/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/kotlinintro/MainActivity.kt
@@ -21,57 +21,12 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(bindingClass.root)
 
-        bindingClass.btnAdd.setOnClickListener {
-
-            val resultValue = bindingClass.editValue.text.toString()
-            Log.d("!!!", "Result = $resultValue")
-            bindingClass.imageView.visibility = View.VISIBLE
-            Log.d("!!!", "ID img-man = ${R.drawable.man}")
-
-            when(resultValue) {
-                Constance.TEACHER -> {
-                    bindingClass.tvResult.visibility = View.VISIBLE
-                    val tempText = "Your salary is ${Constance.TEACHER_SALARY} kr"
-                    if (bindingClass.editKey.text.toString() == Constance.TEACHER_KEY) {
-                        bindingClass.tvResult.text = tempText
-                        bindingClass.imageView.setImageResource(R.drawable.woman)
-                    } else {
-                        bindingClass.tvResult.text = "Incorrect"
-                    }
-                }
-                Constance.ACCOUNTANT -> {
-                    bindingClass.tvResult.visibility = View.VISIBLE
-                    val tempText = "Your salary is ${Constance.ACCOUNTANT_SALARY} kr"
-                    if (bindingClass.editKey.text.toString() == Constance.ACCOUNTANT_KEY) {
-                        bindingClass.tvResult.text = tempText
-                        bindingClass.imageView.setImageResource(R.drawable.job)
-                    } else {
-                        bindingClass.tvResult.text = "Incorrect"
-                    }
-                }
-                Constance.DEVELOPER -> {
-                    bindingClass.tvResult.visibility = View.VISIBLE
-                    val tempText = "Your salary is ${Constance.DEVELOPER_SALARY} kr"
-                    if (bindingClass.editKey.text.toString() == Constance.DEVELOPER_KEY) {
-                        bindingClass.tvResult.text = tempText
-                        bindingClass.imageView.setImageResource(R.drawable.developer)
-                    } else {
-                        bindingClass.tvResult.text = "Incorrect"
-                    }
-                }
-
-                else -> {
-                    bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Can't be empty"
-                }
-            }
-
-        }
     }
 }
 
 /*
-* Link -> https://www.youtube.com/watch?v=YJSidfZBAAk
+* Link -> https://www.youtube.com/watch?v=nnTUXD0-U7A
+* Link -> https://developer.android.com/guide/topics/resources/providing-resources?hl=ru
 * buildFeatures {
        	 viewBinding = true
 * }
