e135019bd17160742021ef0bd0e709228f792b91
Jonas N <jonasniyazson@hotmail.com>
2024-12-20 09:17:41 +0100

    Bismillah -> Alhamdu lillah -> Added Consts

---
+++ app/src/main/java/com/example/kotlinintro/constance/Constance.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinintro.constance
+
+object Constance {
+    const val TEACHER = "Karima"
+    const val ACCOUNTANT = "Sara"
+    const val DEVELOPER = "Jonas"
+
+    const val TEACHER_SALARY = 15000
+    const val ACCOUNTANT_SALARY = 25000
+    const val DEVELOPER_SALARY = 35000
+
+    const val TEACHER_KEY = "2005"
+    const val ACCOUNTANT_KEY = "1976"
+    const val DEVELOPER_KEY = "1969"
+}
