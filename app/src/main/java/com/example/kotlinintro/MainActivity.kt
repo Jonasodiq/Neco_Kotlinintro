989a0b2743138e40d816854482216d20625b2c08
Jonas N <jonasniyazson@hotmail.com>
2024-12-20 15:53:11 +0100

    Bismillah -> Alhamdu lillah -> Added Listener

--- app/src/main/java/com/example/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/kotlinintro/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinintro
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -22,10 +23,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingClass.root)
 
     }
+
+    fun onClickTest2(view: View) {
+        val intent = Intent(this, TestActivity2::class.java)
+        startActivity(intent)
+    }
+
+    fun onClockBack(view: View) {
+        finish()
+    }
 }
 
 /*
-* Link -> https://www.youtube.com/watch?v=nnTUXD0-U7A
+* Link -> https://www.youtube.com/watch?v=kDUIlwkpBaQ
 * Link -> https://developer.android.com/guide/topics/resources/providing-resources?hl=ru
 * buildFeatures {
        	 viewBinding = true
