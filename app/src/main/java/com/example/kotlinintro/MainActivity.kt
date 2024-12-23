b2aee7f26ed021e16a588d1e9c9434e4de8eb327
Jonas N <jonasniyazson@hotmail.com>
2024-12-23 18:48:41 +0100

    Bismillah -> Alhamdu lillah -> Added Main

--- app/src/main/java/com/example/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/kotlinintro/MainActivity.kt
@@ -3,18 +3,48 @@ package com.example.kotlinintro
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.helper.widget.Grid
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.kotlinintro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter = PlantAdapter()
+    private val imageIdList = listOf(
+        R.drawable.img1,
+        R.drawable.img2,
+        R.drawable.img3,
+        R.drawable.img4,
+        R.drawable.img5,
+        R.drawable.img6,
+    )
+    private var index = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rcView)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+            binding.recyclerView.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            binding.recyclerView.adapter = adapter
+            buttonAdd.setOnClickListener {
+                if(index > 5) index = 0
+                val plant = Plant(imageIdList[index], "Plant $index")
+                adapter.addPlant(plant)
+                index++
+            }
         }
     }
 }
