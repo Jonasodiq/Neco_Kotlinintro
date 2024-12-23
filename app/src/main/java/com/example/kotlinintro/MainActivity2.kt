ee5f80ee381ac2400d741974153f6cfd0abd2478
Jonas N <jonasniyazson@hotmail.com>
2024-12-23 09:52:27 +0100

    Bismillah -> Alhamdu lillah -> Added Main2

---
+++ app/src/main/java/com/example/kotlinintro/MainActivity2.kt
@@ -0,0 +1,29 @@
+package com.example.kotlinintro
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onClick(view: View) {
+        val i = Intent()
+        i.putExtra("key1", "done")
+        setResult(RESULT_OK, i)
+        finish()
+    }
+}
