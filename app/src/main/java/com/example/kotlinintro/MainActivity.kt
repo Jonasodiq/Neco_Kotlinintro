88dd0fa3d8a8728402dc51da2fcf9d305455c693
Jonas N <jonasniyazson@hotmail.com>
2024-12-23 08:45:32 +0100

    Bismillah -> Alhamdu lillah -> Added Pithogor

--- app/src/main/java/com/example/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/kotlinintro/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.kotlinintro
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinintro.databinding.ActivityMainBinding
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+    }
+
+    fun onClickResult(view: View){
+        if(!isFieldEmpty()){
+            val result = getString(R.string.result_info) + getResult()
+            binding.tvResult.text = result
         }
     }
+
+    private fun isFieldEmpty(): Boolean{
+        binding.apply {
+            if(edA.text.isNullOrEmpty()) edA.error = "Поле должно быть заполнено"
+            if(edB.text.isNullOrEmpty()) edB.error = "Поле должно быть заполнено"
+            return  edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
+        }
+    }
+
+    private fun getResult(): String{
+        val a: Double
+        val b: Double
+        binding.apply {
+            a = edA.text.toString().toDouble()
+            b = edB.text.toString().toDouble()
+        }
+        return sqrt((a.pow(2) + b.pow(2))).toString()
+    }
 }
+
