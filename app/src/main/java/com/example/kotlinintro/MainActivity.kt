b6795893113e0d33957d5b7a2b642e58722c9f73
Jonas N <jonasniyazson@hotmail.com>
2024-12-23 09:52:18 +0100

    Bismillah -> Alhamdu lillah -> Added Main

--- app/src/main/java/com/example/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/kotlinintro/MainActivity.kt
@@ -1,20 +1,55 @@
 package com.example.kotlinintro
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var launcher: ActivityResultLauncher<Intent>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        launcher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+
+                if (result.resultCode == RESULT_OK) {
+                    val text = result.data?.getStringExtra("key1")
+                    Log.d("!!!", "Result from MA2: $text")
+                }
+
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                    v.setPadding(
+                        systemBars.left,
+                        systemBars.top,
+                        systemBars.right,
+                        systemBars.bottom
+                    )
+                    insets
+                }
+            }
+    }
+
+    fun onClick(view: View) {
+        launcher?.launch(Intent(this, MainActivity2::class.java))
+
     }
 }
+
+
+/*
+* Link -> https://developer.android.com/training/basics/intents/result?hl=ru
+* */
