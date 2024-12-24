eb9d7ef7f7f551ed2e9e1740e112f32b398d7a75
Jonas N <jonasniyazson@hotmail.com>
2024-12-24 20:09:17 +0100

    Bismillah -> Alhamdu lillah -> Added main

--- app/src/main/java/com/example/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/kotlinintro/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinintro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btnFrag2.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.place_holder, BlankFragment2.newInstance())
+                .commit()
+        }
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.place_holder, BlankFragment.newInstance())
+            .commit()
     }
 }
+
+/*
+* Link -> https://www.youtube.com/watch?v=6zp-AKBaMT8
+* Link -> https://developer.android.com/guide/fragments?hl=ru
+* Link -> https://developer.android.com/guide/fragments/lifecycle?hl=ru
+*
+* */
