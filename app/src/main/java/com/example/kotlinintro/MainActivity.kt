d99124ba5ce2e4305d3c42d51465bef48944bb77
Jonas N <jonasniyazson@hotmail.com>
2024-12-24 16:50:07 +0100

    Bismillah -> Alhamdu lillah -> Added main

--- app/src/main/java/com/example/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/kotlinintro/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.kotlinintro
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinintro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Aktivera ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Hantera systeminpassningar
+        ViewCompat.setOnApplyWindowInsetsListener(binding.drawer) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Öppna navigation drawer när "Open Menu"-knappen klickas
+        binding.open.setOnClickListener {
+            binding.drawer.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        // Stäng navigation drawer om den är öppen när bakåtknappen trycks
+        if (binding.drawer.isDrawerOpen(GravityCompat.START)) {
+            binding.drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
+
+/*
+* Link -> https://www.youtube.com/watch?v=InA9RaoKpxM
+* Link -> https://developer.android.com/reference/androidx/drawerlayout/widget/DrawerLayout
+*
+* */
