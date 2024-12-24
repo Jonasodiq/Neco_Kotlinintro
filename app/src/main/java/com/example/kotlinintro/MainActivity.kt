6b52775118d6b98381fbea38501470f70224d061
Jonas N <jonasniyazson@hotmail.com>
2024-12-24 16:09:10 +0100

    Bismillah -> Alhamdu lillah -> Added Main

--- app/src/main/java/com/example/kotlinintro/MainActivity.kt
+++ app/src/main/java/com/example/kotlinintro/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.kotlinintro
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinintro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.bNav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.item1 -> {
+                    Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 -> {
+                    Toast.makeText(this, "Item 2", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item3 -> {
+                    Toast.makeText(this, "Item 3", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item4 -> {
+                    Toast.makeText(this, "Item 4", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 }
+
+/*
+*  Link -> https://www.youtube.com/watch?v=KnNjyDkEXu4
+*  Link -> https://developer.android.com/reference/com/google/android/material/bottomnavigation/BottomNavigationView
+* */
